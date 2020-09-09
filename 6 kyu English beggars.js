b0f95ccf6073a645779d50caf25403d363b04343
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-09-09 16:46:41 -0700

    6 kyu English beggars

---
+++ 6 kyu English beggars.js
@@ -0,0 +1,33 @@
+// Born a misinterpretation of this kata, your task here is pretty simple: given an array of values and an amount of beggars, you are supposed to return an array with the sum of what each beggar brings home, assuming they all take regular turns, from the first to the last.
+
+// For example: [1,2,3,4,5] for 2 beggars will return a result of [9,6], as the first one takes [1,3,5], the second collects [2,4].
+
+// The same array with 3 beggars would have in turn have produced a better out come for the second beggar: [5,7,3], as they will respectively take [1,4], [2,5] and [3].
+
+// Also note that not all beggars have to take the same amount of "offers", meaning that the length of the array is not necessarily a multiple of n; length can be even shorter, in which case the last beggars will of course take nothing (0).
+
+// Note: in case you don't get why this kata is about English beggars, then you are not familiar on how religiously queues are taken in the kingdom ;)
+
+function beggars(values, n) {
+	let result = [];
+	let l = values.length;
+	if (n === 0) {
+		return [];
+	} else if (n > l) {
+		for (let i = 0; i < n - l; i++) {
+			values.push(0);
+		}
+		return values;
+	} else if (n === l) {
+		return values;
+	} else {
+		for (let j = 0; j < n; j++) {
+			for (let k = 0; k < l; k++) {
+				if (j === k % n) {
+					result[j] = result[j] + values[k] || values[k];
+				}
+			}
+		}
+		return result;
+	}
+}
